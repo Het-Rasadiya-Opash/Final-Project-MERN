e2d265f4388402e00662536ab29012edf697d373
Het <hra.opash@gmail.com>
2026-02-26 13:43:34 +0530

    .

--- Frontend/src/pages/Profile.tsx
+++ Frontend/src/pages/Profile.tsx
@@ -1,35 +1,67 @@
-import useAuthStore from '../utils/authStore';
-import { User } from 'lucide-react';
+import { useEffect, useState } from "react";
+import useAuthStore from "../utils/authStore";
+import apiRequest from "../utils/apiRequest";
+import Listing from "../components/Listing";
 
 const Profile = () => {
-    const {currentUser} = useAuthStore();
-    
-    if(!currentUser) return <div className="p-8">Please log in to view your profile.</div>;
+  const { currentUser } = useAuthStore();
+  const [listings, setListings] = useState<any>([]);
+  const [loading, setLoading] = useState(true);
+
+  if (!currentUser)
+    return <div className="p-8">Please log in to view your profile.</div>;
+
+  useEffect(() => {
+    const fetchUserListing = async () => {
+      try {
+        const res = await apiRequest.get(`listing/user-listing`);
+        setListings(res.data.data);
+      } catch (error) {
+        console.error("Failed to fetch listing:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchUserListing();
+  }, []);
 
   return (
-    <div className="max-w-4xl mx-auto p-8">
+    <div className="max-w-6xl mx-auto p-8">
       <div className="bg-white shadow rounded-lg p-6">
-        <div className="flex items-center gap-4 mb-6">
-          <div className="bg-blue-100 p-4 rounded-full">
-            <User size={48} className="text-blue-600" />
-          </div>
-          <h1 className="text-3xl font-bold">Profile</h1>
-        </div>
         
+
         <div className="space-y-4">
           <div>
             <label className="text-gray-600 font-semibold">Username</label>
             <p className="text-lg">{currentUser.username}</p>
           </div>
-          
+
           <div>
             <label className="text-gray-600 font-semibold">Email</label>
             <p className="text-lg">{currentUser.email}</p>
           </div>
         </div>
       </div>
+      {
+        loading ? (
+          <div className="text-center py-8">Loading...</div>
+        ) : (
+          <div>
+            <h2 className="text-2xl font-bold mt-8 mb-4">Your Listings</h2>
+            {listings.length === 0 ? (
+              <p className="text-gray-600">No listings found.</p>
+            ) : (
+              <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+                {listings.map((listing: any) => (
+                  <Listing key={listing._id} listing={listing} />
+                ))}
+              </div>
+            )}
+          </div>
+        )
+      }
     </div>
-  )
-}
+  );
+};
 
-export default Profile
+export default Profile;
