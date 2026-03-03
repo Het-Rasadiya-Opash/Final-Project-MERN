bbb754f7095154107acbd027c7a3e82c4c699161
Het <hra.opash@gmail.com>
2026-03-03 10:24:59 +0530

    if listing owner and admin login can't show  booking now btn

--- Frontend/src/pages/ListingPage.tsx
+++ Frontend/src/pages/ListingPage.tsx
@@ -53,6 +53,7 @@ const ListingPage = () => {
 
   const isOwner = currentUser?._id === listing.owner?._id;
 
+
   return (
     <div className="max-w-6xl mx-auto p-4 md:p-6">
       <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
@@ -106,7 +107,9 @@ const ListingPage = () => {
         <div>
           <div className="flex flex-col sm:flex-row sm:items-center sm:justify-between gap-4 mb-4">
             <h1 className="text-2xl md:text-3xl font-bold">{listing.title}</h1>
-            <Link to={`/booking/${listing._id}`}>
+            {
+              (!isOwner && !currentUser?.admin)  && (
+                <Link to={`/booking/${listing._id}`}>
               <button className="flex items-center gap-2 px-4 py-2 bg-blue-50 hover:bg-blue-100 text-blue-600 rounded-lg transition-all duration-200 border border-blue-200">
                 <svg
                   className="w-5 h-5"
@@ -124,6 +127,8 @@ const ListingPage = () => {
                 <span className="font-medium">Book Now</span>
               </button>
             </Link>
+              )
+            }
           </div>
           <p className="text-2xl md:text-3xl font-bold text-blue-600 mb-4">
             ₹{listing.price?.toLocaleString()}
